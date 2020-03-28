10f03352799ce2297320fda817ed8ce6ec481336
Dmitrii Altukhov <dm.altukhov@ya.ru>
2020-03-28 20:47:01 +0300

    Add new folders to config; fix confidence spelling

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 from pathlib import Path
 
+# setup BIDS root folder
 curdir = Path(__file__)
 BIDS_ROOT = curdir.parent.parent
 
+# setup source data folder
 RAW_DIR = BIDS_ROOT.parent / "raw"
 
+# create folder for derivatives
 DERIVATIVES_DIR = BIDS_ROOT / "derivatives"
 DERIVATIVES_DIR.mkdir(exist_ok=True)
 
@@ -12,22 +15,46 @@ DERIVATIVES_DIR.mkdir(exist_ok=True)
 HP_DIR = DERIVATIVES_DIR / "01-head_positon"
 HP_DIR.mkdir(exist_ok=True)
 
-# create folder for head reports
+# create folder for bad channels and bad segments annotations for maxfilter
 BADS_DIR = DERIVATIVES_DIR / "02-maxfilter_bads"
 BADS_DIR.mkdir(exist_ok=True)
 
-# create folder for head reports
+# create folder for maxfiltered data
+MAXFILTER_DIR = DERIVATIVES_DIR / "03-maxfilter"
+MAXFILTER_DIR.mkdir(exist_ok=True)
+
+# create foder for filtered and resampled data
+FILTER_DIR = DERIVATIVES_DIR / "04-concat_filter_resample"
+FILTER_DIR.mkdir(exist_ok=True)
+
+# create folder for ICA solutions
+ICA_SOL_DIR = DERIVATIVES_DIR / "05-compute_ica"
+ICA_SOL_DIR.mkdir(exist_ok=True)
+
+# create folder for ICA solutions
+ICA_DIR = DERIVATIVES_DIR / "06-apply_ica"
+ICA_DIR.mkdir(exist_ok=True)
+
+# create folder for epoched data
+EPOCHS_DIR = DERIVATIVES_DIR / "06-make_epochs"
+EPOCHS_DIR.mkdir(exist_ok=True)
+
+# create folder for reports
 REPORTS_DIR = DERIVATIVES_DIR / "99-reports"
 REPORTS_DIR.mkdir(exist_ok=True)
 
+crosstalk_file = str(BIDS_ROOT / "SSS_data" / "ct_sparse.fif")
+cal_file = str(BIDS_ROOT / "SSS_data" / "sss_cal.dat")
 
 ev_id = {
     "question/second": 1,
     "question/third": 2,
     "fixcross": 3,
     "answer": 4,
-    "confidense": 5,
+    "confidence": 5,
 }
+
+random_state = 28
 
 if __name__ == "__main__":
     print(f"Current path is {curdir}")
