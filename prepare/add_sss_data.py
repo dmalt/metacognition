2a33106800d436354c99c6b66f2a2508ee819789
Dmitrii Altukhov <dm.altukhov@ya.ru>
2021-11-06 14:11:49 +0300

    add script for adding crosstalk and calibration

---
+++ prepare/add_sss_data.py
@@ -0,0 +1,13 @@
+"""Add calibration and crosstalk files to data repository"""
+from tqdm import tqdm
+from mne_bids import write_meg_calibration, write_meg_crosstalk, BIDSPath
+from metacog.config_parser import cfg
+from metacog import bp
+from metacog.paths import crosstalk, calibration, dirs
+
+
+sss_data = BIDSPath(root=dirs.bids_root)
+for subj in tqdm(cfg.all_subjects):
+    sss_data.update(subject=subj)
+    write_meg_calibration(calibration, sss_data)
+    write_meg_crosstalk(crosstalk, sss_data)
